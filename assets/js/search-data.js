11918965166e14c827224684a3c8a6717ef7b5e4
alejandro-sarria-morales <143464834+alejandro-sarria-morales@users.noreply.github.com>
2025-11-04 06:06:25 +0000

    Deploying to gh-pages from @ alejandro-sarria-morales/alejandro-sarria-morales.github.io@294275d0b6d2bd6418ea225c7687c15da5165ac9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
